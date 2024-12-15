8138f53a79906f9eb70cbe7da90e496c17c91402
lis3809 <lis3809@yandex.ru>
2024-12-15 22:47:13 +0300

    update

--- space_game.py
+++ space_game.py
@@ -75,8 +75,45 @@ class SpaceGame():
             self.all_sprites.add(asteroid)
             self.asteroids.add(asteroid)
 
+    def check_collision(self):
+        # Проверяем столкновение игрока с астероидом
+        list_colid = pg.sprite.spritecollide(self.spaceship, self.asteroids, False)
+        if len(list_colid) > 0:
+            if self.__game_dialog.show_dialog_game_over():
+                self.__init_game()
+            else:
+                exit()
 
+        # Если астероид вылетел за край экрана
+        for asteroid in self.asteroids:
+            if asteroid.rect.y > self.screen.get_height():
+                self.__current_player_score += 1
+                self.asteroids.remove(asteroid)
+                self.all_sprites.remove(asteroid)
 
+                # Увеличваем количество астероидов
+                if self.__current_player_score % 3 == 0:
+                    self.count_asteroid += 1
+
+        # Если количество астероидов уменьшилось
+        if len(self.asteroids) < self.count_asteroid:
+            # Объект астероида
+            newAsteroid = Asteroid(self.screen)
+            self.all_sprites.add(newAsteroid)
+            self.asteroids.add(newAsteroid)
+
+    def __draw_score(self):
+        # Надпись с именем игрока
+        # Шрифт и размер текста
+        font = pg.font.Font(None, 28)
+        text_name = font.render(f"Игрок: {self.__player_name}", True, 'white')
+        text_name_rect = text_name.get_rect(topleft=(10, 30))
+        self.screen.blit(text_name, text_name_rect)
+
+        # Надпись с текущими очками игрока
+        text_score = font.render(f"Очки: {self.__current_player_score}", True, 'white')
+        text_score_rect = text_score.get_rect(topleft=(10, 50))
+        self.screen.blit(text_score, text_score_rect)
 
     def __draw_scene(self):
         # отрисовка
